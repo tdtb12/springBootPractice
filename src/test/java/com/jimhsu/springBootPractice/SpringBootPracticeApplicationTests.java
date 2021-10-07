6ded3c2028f1cafec5266db74a6de34618ef20b7
Jim <tdtb12@gmail.com>
2021-10-07 17:22:16 +0800

    initializing

---
+++ src/test/java/com/jimhsu/springBootPractice/SpringBootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimhsu.springBootPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
