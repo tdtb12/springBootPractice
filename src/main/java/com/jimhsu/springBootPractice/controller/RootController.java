7a1e598eacee6a77bbc7140d031ad6bf2c2224f7
Jim <tdtb12@gmail.com>
2021-10-08 15:05:02 +0800

    - add welcome page

---
+++ src/main/java/com/jimhsu/springBootPractice/controller/RootController.java
@@ -0,0 +1,14 @@
+package com.jimhsu.springBootPractice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RootController {
+
+    @RequestMapping("/")
+    public String IndexController() {
+        return "index.html";
+    }
+
+}
